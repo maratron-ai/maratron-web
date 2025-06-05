bddead81078be04fab707cb66090587568df4d36
Jackson Thetford <111920176+jthet@users.noreply.github.com>
2025-06-05 17:29:27 -0500

    Add pages for creating runs and shoes

---
+++ src/app/(auth)/runs/new/page.tsx
@@ -0,0 +1,10 @@
+import CreateRun from "@components/CreateRun";
+
+const NewRunPage = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold mb-4">Add a Run</h1>
+    <CreateRun />
+  </div>
+);
+
+export default NewRunPage;
---
+++ src/app/(auth)/shoes/new/page.tsx
@@ -0,0 +1,10 @@
+import CreateShoe from "@components/CreateShoe";
+
+export default function NewShoePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Add a Shoe</h1>
+      <CreateShoe />
+    </div>
+  );
+}
