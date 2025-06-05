cfcb50e1e277d6b272f9b7b487086e2da32ce1c1
Jackson Thetford <111920176+jthet@users.noreply.github.com>
2025-06-04 20:45:41 -0500

    fix tests and user api param

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
