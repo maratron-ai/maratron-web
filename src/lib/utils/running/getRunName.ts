9d9127b9bc3d52322600646061cdeb45116ad7cc
Jackson Thetford <j.thetford@utexas.edu>
2025-06-06 22:49:20 -0500

    naming runs

--- src/lib/utils/running/getRunName.ts
+++ src/lib/utils/running/getRunName.ts
@@ -4,6 +4,6 @@ export const getRunName = (run: Pick<Run, 'date' | 'trainingEnvironment'>): stri
   const dt = new Date(run.date);
   const datePart = dt.toLocaleDateString();
   const timePart = dt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  const env = run.trainingEnvironment ?? 'unspecified';
-  return `${datePart} ${timePart} - ${env? env : 'run'}`.trim();
+  const env = run.trainingEnvironment ?? 'run';
+  return `${datePart} ${timePart} - ${env}`.trim();
 };
