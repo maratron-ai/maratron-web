f508772571cdf5a504adee8ba9a595deaef24efc
Jackson Thetford <111920176+jthet@users.noreply.github.com>
2025-06-10 16:12:42 -0500

    Add styled landing page

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@lib/utils/cn";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent bg-secondary text-white",
+        outline: "border border-border bg-transparent",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof badgeVariants> {}
+
+export const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <span
+        ref={ref}
+        className={cn(badgeVariants({ variant }), className)}
+        {...props}
+      />
+    );
+  }
+);
+Badge.displayName = "Badge";
+
+export { badgeVariants };
