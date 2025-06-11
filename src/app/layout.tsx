2341915f124424d1d7445bbb32fd016524945ecb
Jackson Thetford <111920176+jthet@users.noreply.github.com>
2025-06-10 21:12:16 -0500

    Refactor layout and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@components/Providers";
 import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
