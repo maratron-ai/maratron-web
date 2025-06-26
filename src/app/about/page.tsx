e639b7fc52b98bfc491e41750019e7355137fc22
Jackson Thetford <j.thetford@utexas.edu>
2025-06-26 18:31:21 -0500

    update to trigger ci

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
       <h1 className="text-3xl font-bold">About Maratron</h1>
       <p className="text-lg text-center">
         Maratron is your AI-powered running coach, helping you train smarter,
-        stay motivated, and achieve your goals—one step at a time!
+        stay motivated, and achieve your goals—one step at a time
       </p>
     </main>
   );
